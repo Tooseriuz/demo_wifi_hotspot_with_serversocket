bfb6518640d3d0c2a8e2016aac2f0f8c19a05734
wasuphon <wasuphonn59@email.nu.ac.th>
2020-07-17 18:40:55 +0700

    Add audio streaming

--- socketclient/src/main/java/com/wasuphon/poc/socketclient/MainActivity.kt
+++ socketclient/src/main/java/com/wasuphon/poc/socketclient/MainActivity.kt
@@ -1,27 +1,28 @@
 package com.wasuphon.poc.socketclient
 
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioTrack
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.DataInputStream
-import java.io.DataOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
-import java.net.ServerSocket
+import java.lang.Exception
 import java.net.Socket
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var socket: Socket
-    private lateinit var stream: DataOutputStream
+    private lateinit var stream: DataInputStream
     private lateinit var serverThread: Thread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        sendData(JSONObject())
 
         prefBtn.setOnClickListener {
             val prefsData = JSONObject()
@@ -59,62 +60,87 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendData(dataForSend: JSONObject) {
+        val frequency = 44100
+        val channelConfiguration = AudioFormat.CHANNEL_CONFIGURATION_MONO
+        val audioEncoding = AudioFormat.ENCODING_PCM_16BIT
+        var socket: Socket
+
+        val playBufSize = AudioTrack.getMinBufferSize(
+            frequency,
+            channelConfiguration,
+            audioEncoding
+        )
+        val audioTrack = AudioTrack(
+            AudioManager.STREAM_MUSIC,
+            frequency,
+            channelConfiguration,
+            audioEncoding,
+            playBufSize,
+            AudioTrack.MODE_STREAM
+        )
+        audioTrack.setStereoVolume(1f, 1f);
+
+        val buffer = ByteArray(playBufSize)
         serverThread = Thread(Runnable {
-            socket = Socket("192.168.1.83", 9090)
+            socket = Socket("192.168.43.1", 9090)
 
+            audioTrack.play()
             while (true) {
-                stream = DataOutputStream(socket.getOutputStream())
-
-                stream.writeUTF(dataForSend.toString())
-                stream.flush()
-
-                //receiving data
-                val data = DataInputStream(socket.getInputStream()).readUTF()
-                if (!data.isNullOrBlank()) {
-
-                    Log.d("ClientSocket", "Receiving data from Server: $data")
-
-                    val receiveJson = JSONObject(data)
-                    val jsonIterator = receiveJson.keys()
-
-                    val keys = jsonIterator.asSequence().toList()
-                    when (keys[0].toString()) {
-                        "response" -> {
-                            Log.d(
-                                "ClientSocket",
-                                "Response from server: ${receiveJson.get("response")}"
-                            )
-                            runOnUiThread{
-                                responseTV.text = receiveJson.get("response").toString()
-                            }
-                        }
-                        "diagnostic" -> {
-                            val responseData = JSONObject(receiveJson.get("diagnostic").toString())
-                            var responseForShow = ""
-                            responseData.keys().forEach {
-                                when (it) {
-                                    "battery" -> Log.d(
-                                        "ClientSocket",
-                                        "Guardian battery: ${responseData.getInt(it)}"
-                                    )
-                                    "total_record" -> Log.d(
-                                        "ClientSocket",
-                                        "Guardian total record: ${responseData.getInt(it)}"
-                                    )
-                                    "total_size" -> Log.d(
-                                        "ClientSocket",
-                                        "Guardian total size: ${responseData.getInt(it)}"
-                                    )
-                                }
-                                responseForShow += "\n Guardian $it: ${responseData.getInt(it)}"
-                            }
-                            runOnUiThread {
-                                responseTV.text = responseForShow
-                            }
-                        }
-                    }
-                }
+                var readSize = 0
+                try {
+                    stream = DataInputStream(socket.getInputStream())
+                    readSize = stream.read(buffer)
+                } catch (e: Exception) {}
+                audioTrack.write(buffer, 0, readSize)
             }
+
+            //receiving data
+//                val data = DataInputStream(socket.getInputStream()).readUTF()
+//                if (!data.isNullOrBlank()) {
+//
+//                    Log.d("ClientSocket", "Receiving data from Server: $data")
+
+//                    val receiveJson = JSONObject(data)
+//                    val jsonIterator = receiveJson.keys()
+//
+//                    val keys = jsonIterator.asSequence().toList()
+//                    when (keys[0].toString()) {
+//                        "response" -> {
+//                            Log.d(
+//                                "ClientSocket",
+//                                "Response from server: ${receiveJson.get("response")}"
+//                            )
+//                            runOnUiThread{
+//                                responseTV.text = receiveJson.get("response").toString()
+//                            }
+//                        }
+//                        "diagnostic" -> {
+//                            val responseData = JSONObject(receiveJson.get("diagnostic").toString())
+//                            var responseForShow = ""
+//                            responseData.keys().forEach {
+//                                when (it) {
+//                                    "battery" -> Log.d(
+//                                        "ClientSocket",
+//                                        "Guardian battery: ${responseData.getInt(it)}"
+//                                    )
+//                                    "total_record" -> Log.d(
+//                                        "ClientSocket",
+//                                        "Guardian total record: ${responseData.getInt(it)}"
+//                                    )
+//                                    "total_size" -> Log.d(
+//                                        "ClientSocket",
+//                                        "Guardian total size: ${responseData.getInt(it)}"
+//                                    )
+//                                }
+//                                responseForShow += "\n Guardian $it: ${responseData.getInt(it)}"
+//                            }
+//                            runOnUiThread {
+//                                responseTV.text = responseForShow
+//                            }
+//                        }
+//                    }
+//                }
+
         })
 
         serverThread.start()
--- socketserver/src/main/java/com/wasuphon/poc/socketserver/MainActivity.kt
+++ socketserver/src/main/java/com/wasuphon/poc/socketserver/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.wasuphon.poc.socketserver
 
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder.AudioSource
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONArray
 import org.json.JSONObject
 import java.io.DataInputStream
 import java.io.DataOutputStream
+import java.lang.Exception
 import java.net.InetSocketAddress
 import java.net.ServerSocket
 import java.net.Socket
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,48 +42,72 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startServer() {
+        val sampleRate = 44100
+        val channelConfig: Int = AudioFormat.CHANNEL_CONFIGURATION_MONO
+        val audioFormat: Int = AudioFormat.ENCODING_PCM_16BIT
+        val minBufSize = AudioRecord.getMinBufferSize(
+            sampleRate, channelConfig,
+            audioFormat
+        )
+        val recorder = AudioRecord(
+            AudioSource.DEFAULT,
+            sampleRate, channelConfig, audioFormat,
+            minBufSize
+        )
         clientThread = Thread(Runnable {
             serverSocket = ServerSocket()
             serverSocket.reuseAddress = true
             serverSocket.bind(InetSocketAddress(9090))
+            val buffer = ByteArray(minBufSize)
 
             while (true) {
                 socket = serverSocket.accept()
 
-                stream = DataInputStream(socket.getInputStream())
+                recorder.startRecording()
+                while (true) {
+                    val readSize = recorder.read(buffer, 0, minBufSize);
+                    try {
+                        socket.getOutputStream().write(buffer, 0, readSize);
 
-                val data = stream.readUTF()
-
-                //send same data back
-                if (!data.isNullOrBlank()) {
-                    Log.d("ServerSocket", "Receiving data from Client: $data")
-
-                    val receiveJson = JSONObject(data)
-                    val jsonIterator = receiveJson.keys()
-                    jsonIterator.next()
-
-                    val streamOut = DataOutputStream(socket.getOutputStream())
-
-                    streamOut.flush()
-                    when (receiveJson.get("command")) {
-                        "prefs" -> {
-                            jsonIterator.forEach {
-                                setPrefs(it, receiveJson.get(it).toString())
-                            }
-                            sendSuccess(streamOut)
-                        }
-                        "reboot" -> {
-                                control(receiveJson.get("command").toString())
-                                sendSuccess(streamOut)
-                        }
-                        "diagnostic" -> {
-                                val diagnostic = getDiagnostic()
-                                streamOut.writeUTF(diagnostic.toString())
-                                streamOut.flush()
-                        }
                     }
+                    catch (e: Exception) { }
                 }
             }
+
+
+            //send same data back
+//                if (!data.isNullOrBlank()) {
+//                    Log.d("ServerSocket", "Receiving data from Client: $data")
+//
+//                    val receiveJson = JSONObject(data)
+//                    val jsonIterator = receiveJson.keys()
+//                    jsonIterator.next()
+//
+//                    val streamOut = DataOutputStream(socket.getOutputStream())
+//
+//                    streamOut.flush()
+//                    when (receiveJson.get("command")) {
+//                        "prefs" -> {
+//                            jsonIterator.forEach {
+//                                setPrefs(it, receiveJson.get(it).toString())
+//                            }
+//                            sendSuccess(streamOut)
+//                        }
+//                        "reboot" -> {
+//                                control(receiveJson.get("command").toString())
+//                                sendSuccess(streamOut)
+//                        }
+//                        "diagnostic" -> {
+//                                val diagnostic = getDiagnostic()
+//                                streamOut.writeUTF(diagnostic.toString())
+//                                streamOut.flush()
+//                        }
+//                        "audio" -> {
+//
+//                        }
+//                    }
+//                }
+
         })
 
         clientThread.start()
@@ -96,7 +124,7 @@ class MainActivity : AppCompatActivity() {
     private fun getDiagnostic(): JSONObject {
         val diagnostic = JSONObject()
         val value = JSONObject()
-        value.put("battery" , 100)
+        value.put("battery", 100)
         value.put("total_record", 3600)
         value.put("total_size", 10000)
 
